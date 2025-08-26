08c75ee374bd53b8b2ed9cc76d34a210bc9b3618
jhonjel <jhonjadere99@gmail.com>
2025-08-26 14:25:58 -0500

    Add side menu navigation to Ionic app with menu buttons on all tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+
+import { IonApp, IonRouterOutlet, IonMenu, IonToolbar, IonHeader, IonTitle, IonList, IonContent, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [ IonLabel, IonItem, IonContent, IonList, IonTitle, IonHeader, IonToolbar, IonApp, IonRouterOutlet, IonMenu,],
 })
 export class AppComponent {
+  menuCtrl: any;
   constructor() {}
+
+  openMenu() {
+    this.menuCtrl.open().then(() => {
+      if (document.activeElement instanceof HTMLElement) {
+        document.activeElement.blur();
+      }
+    });
+  }
+
+
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonMenuButton]
 })
 export class Tab2Page {
 
