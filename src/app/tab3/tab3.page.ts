09be47e46185deadfc7ef9e9dc3d6220b13963bf
jhonjel <jhonjadere99@gmail.com>
2025-09-02 15:57:48 -0500

    subida completa Semana4

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonAccordionGroup, IonAccordion, IonItem, IonLabel, IonButton, } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonMenuButton],
+  imports: [ IonButton, IonLabel, IonItem, IonAccordion, IonAccordionGroup, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonMenuButton, RouterModule],
 })
 export class Tab3Page {
   constructor() {}
